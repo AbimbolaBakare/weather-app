2223e99abfe347b3606ec24157b76388d571dccf
Abimbola Bakare <abimbola.bakare@duronetworks.com>
2020-12-09 14:59:18 +0100

    correct build errors

--- src/fetchWeather.js
+++ src/fetchWeather.js
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from "react";
-import { createMuiTheme} from "@material-ui/core";
 import CircularProgress from "@material-ui/core/CircularProgress";
 import Weather from "./components/Weather";
 import NavBar from "./components/Navbar";
